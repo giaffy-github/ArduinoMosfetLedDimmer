fa578fc694951799260792bcc754d0bcf22fe3e6
roby <giaffy@gmail.com>
2020-09-20 11:11:14 +0200

    use new sensor code
    use new sensor code: ADCTouch;
    prev. code CapacitiveSensor had problem with heart ground
    not connected with device, which results in false positives;
    No use ADCTouch source which implements different logic
    and uses internal capacitor in analog pin A0;
    
    see https://github.com/martin2250/ADCTouch

--- MosfetLedDimmer.ino
+++ MosfetLedDimmer.ino
@@ -8,11 +8,11 @@
 
 //
 // sleep / powerdown header
-#include "LowPower.h"
+//#include "LowPower.h"
 
 //
 // capacitive sensor header
-#include "CapacitiveSensor.h"
+#include "ADCTouch.h"
 
 //
 // enable debug printout
@@ -42,7 +42,7 @@
 
 //
 // program version
-const String currVersion = "v20200917";
+const String currVersion = "v20200919";
 
 //
 // fade delay in main loop
@@ -53,13 +53,11 @@ const int delayFadingLoop = 30;   // 25 millisec. used in LED fading in/out
 const int mosfetPin = 9;
 
 //
-// pin layout: capacity sensor out and int
-const int capacitySensorOutPin = 4;
-const int capacitySensorInPin = 2;
+// pin layout: capacity sensor A0
 
 //
 // capacitive sensor threshold (value greater than this trigger the state to pressed)
-const int sensorThreshold = 1400;
+const int sensorThreshold = 40;
 
 //
 //
@@ -191,13 +189,11 @@ protected:
 //
 // global var
 
-// capacitive sensor ports pair: pin 4 out signal, 2 reading capacitance
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(capacitySensorOutPin,capacitySensorInPin);
-                          // pin 4 === 1M resistor === foil === 10 K resistor === pin 2
-                          // pin 4 out, pin 2 is sensor pin,
-                          // 100 pF on pin 2 and ground to stabilize reading;
-
 Led ledObj;
+
+int counterReading = 0;
+
+int sensorRef0 = 0;     //reference values to remove offset
 
 void setup() {
 
@@ -225,15 +221,15 @@ void setup() {
   // capacity sensor threshold
   print_debugnln( "capacity sensor threshold number: " );
   print_debug( sensorThreshold );
-  print_debugnln( "capacity sensor out pin: " );
-  print_debug( capacitySensorOutPin );
-  print_debugnln( "capacity sensor in pin: " );
-  print_debug( capacitySensorInPin );
+  print_debug( "capacity sensor pin: A0" );
 
   //
   // capacitor sensor
-  print_debug( "init capacity sensor lib ..." );
-  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  print_debug( "create reference values to account for the capacitance of the pad ..." );
+  sensorRef0 = ADCTouch.read(A0, 500);    //create reference values to account for the capacitance of the pad
+
+  print_debugnln( "capacitance reference: " );
+  print_debug(sensorRef0);
 
   print_debug( "app ready ..." );
 }
@@ -247,47 +243,40 @@ void loop() {
 
     // value returned by capacitive sensor button: needs to be static as
     // reading is done avery 10th iterations to limit the time consumed;
-    // last reading is kepr till next reading, and fading take place if required
-    static long sensorVal = 0;
+    // last reading is kept till next reading, and fading take place if required
+    static int sensorValue0 = 0;
 
-    const int idleReadingStep = 7; // do not read the sensor for x cycle
+    const int idleReadingStep = 10; // do not read the sensor for x cycle
     
     if(counterReading >= idleReadingStep) {
-
-      static int sensorMax = 0;
       
-      counterReading = 0;
-      long sensorStart = millis();
-      sensorVal =  cs_4_2.capacitiveSensor(30);
-
-      long currentTime = millis();
-      Serial.print(currentTime - sensorStart);  // check on performance in milliseconds
-      Serial.print("\t");                    // tab character for debug windown spacing
-
-      Serial.print(sensorVal);               // print sensor output
-      Serial.print("\t\n");
-
-      if(sensorVal > sensorMax) {
-        sensorMax = sensorVal;
-      }
-
-      Serial.print("max val: ");
-      Serial.print(sensorMax);
-      Serial.print("\n");
+        counterReading = 0; // restart counter
+        long sensorStart = millis();  // get initial time
+  
+        sensorValue0 = ADCTouch.read(A0, 75);   // read pin A0, 75 samples
+        sensorValue0 -= sensorRef0;       //remove offset
+  
+        long currentTime = millis();  // get current time
+        Serial.print(currentTime - sensorStart);  // check on performance in milliseconds
+        Serial.print("\t");
+  
+        Serial.print(sensorValue0);               // print sensor value
+        Serial.print("\t\n");
     }
 
-    if( sensorVal > sensorThreshold /* button Pressed */ ) {
+    if( sensorValue0 > sensorThreshold /* button Pressed */ ) {
 
-      ledObj.fadeInOut();
+        ledObj.fadeInOut();
     }
 
     //
     // delay for fading effect: keep into account delay to read sensor
     // and try to delay the same time interval
     long afterAdjust = millis();
+
     if(afterAdjust - loopStart < delayFadingLoop ) {
 
-      long actualDelay = delayFadingLoop - (afterAdjust - loopStart);
-      delay(actualDelay);
+        long actualDelay = delayFadingLoop - (afterAdjust - loopStart);
+        delay(actualDelay);
     }
 }
